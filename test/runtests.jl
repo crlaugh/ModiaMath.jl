01acd45af4e0d5b07bada32bece47564a91870e8
Chris Laughman <crlaugh@gmail.com>
2018-08-23 11:42:34 -0400

    Updating coverage support.

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,7 @@ end
     include(joinpath("result", "_includes.jl"))
     include(joinpath("variables", "_includes.jl"))
     include(joinpath("frames", "_includes.jl"))
-    include(joinpath("simulation", "_includes.jl"))
+#    include(joinpath("simulation", "_includes.jl"))
 end
 
 end
